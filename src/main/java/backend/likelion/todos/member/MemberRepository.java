7c794aceb03bad7bd22fa2c7bbbbada72ae0365a
sslynn22 <129486529+sslynn22@users.noreply.github.com>
2024-03-12 23:27:56 +0900

    feat:level1

--- src/main/java/backend/likelion/todos/member/MemberRepository.java
+++ src/main/java/backend/likelion/todos/member/MemberRepository.java
@@ -13,28 +13,28 @@ public class MemberRepository {
 
     // 멤버를 저장소에 저장하고 저장된 멤버를 반환합니다.
     public Member save(Member member) {
-        // TODO [1단계] member의 id를 설정하세요.
-        // TODO [1단계] members 맵에 member를 추가하세요.
-        // TODO [1단계] member를 반환하세요.
-        return null;
+        member.setId(id);
+        members.put(id, member);
+        id++;
+        return member;
     }
 
     // 주어진 id에 해당하는 멤버를 찾아 Optional로 반환합니다.
     public Optional<Member> findById(Long id) {
-        // TODO [1단계] id를 이용하여 members 맵에서 멤버를 찾으세요.
-        // TODO [1단계] 찾은 멤버를 Optional로 감싸서 반환하세요.
-        return null;
+        Member member = members.get(id);
+        return Optional.ofNullable(member);
     }
 
     // 주어진 username과 일치하는 멤버를 찾아 Optional로 반환합니다.
     public Optional<Member> findByUsername(String username) {
-        // TODO [1단계] members 맵에서 username이 일치하는 멤버를 스트림을 사용해 찾으세요.
-        // TODO [1단계] 찾은 멤버를 Optional로 감싸서 반환하세요.
-        return null;
+        return members.values().stream()
+            .filter(member -> member.getUsername().equals(username))
+            .findFirst();
     }
 
     // 저장소의 모든 멤버를 제거합니다.
     public void clear() {
-        // TODO [1단계] members 맵의 모든 내용을 제거하세요.
+        members.clear();
     }
 }
+
